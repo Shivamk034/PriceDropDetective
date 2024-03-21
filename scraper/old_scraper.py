5d62c48f8e16b9e2728350ade4490fe63f9b4802
shivam <shivamkala2001@gmail.com>
2024-03-21 18:50:22 +0530

    trying with  headless off

--- scraper/old_scraper.py
+++ scraper/old_scraper.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 
 
 chrome_options = webdriver.ChromeOptions() # Create object ChromeOptions()
-chrome_options.add_argument('--headless')           
+# chrome_options.add_argument('--headless')           
 chrome_options.add_argument('--no-sandbox')                             
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--log-level=3")
