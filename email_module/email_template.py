1354de19815736a3acf70e96c0f79325fe771352
Shivam Kala <123884748+Shivamk034@users.noreply.github.com>
2024-06-09 13:55:13 +0000

    update email format

--- email_module/email_template.py
+++ email_module/email_template.py
@@ -1,12 +1,17 @@
+import jinja2
+
+
+with open("email_module/email_template.html","r") as f:
+    _html = f.read()
+_environment = jinja2.Environment()
+template = _environment.from_string(_html)
+
 def get_template_price_drop_email(name,product_name,product_url,previous_price,new_price,product_detail_url):
 
     prod_name_len = 20
     subject = f""" Price Drop Alert!  | {product_name[:prod_name_len]+("..." if len(product_name)>prod_name_len else "")}"""
     
-    with open("email_module/email_template.html","r") as f:
-        html = f.read()
-
-    body = html.format(
+    body = template.render(
         name=name,
         product_name=product_name,
         product_url=product_url,
